f54775775044b562b18dc28683e9fc446c15e1ce
omkarpathak <opathak@uci.edu>
2024-02-20 23:39:11 -0800

    Updated code to be compatible with Boostrap 4.1 and code cleanup

--- js/SiteContent.js
+++ js/SiteContent.js
@@ -1,117 +1,115 @@
 var Pages  = [ "Index",  "About", "Content"]  ;
 
 var SiteContent  = {
-	"About": {	"Header" : `Headeline 1 `,
-				"Body": 
-				` 
-				<p> 
-					Omkar is an impact driven engineer, innovator and builder seeking and shaping the evolving role of technology on people and processes. He is currently researching the democratization of information and resources enabled by blockchains. He's <a href="https://a16z.com/the-techno-optimist-manifesto/">techno-optimist</a>, if you will, and believes the upcoming generation of internet (aka web3) is de-cenentralized, secure and human-centric.
-				</p>
+	"About": {
+		"Body": 
+		` 
+		<p> 
+			Omkar is an impact driven engineer, innovator and builder seeking and shaping the evolving role of technology on people and processes. He is currently researching the democratization of information and resources enabled by blockchains. He's <a href="https://a16z.com/the-techno-optimist-manifesto/">techno-optimist</a>, if you will, and believes the upcoming generation of internet (aka web3) is de-cenentralized, secure and human-centric.
+		</p>
 
-				<p>
-					Based in Seattle, Omkar contributes to data center automations at Amazon Web Services (AWS). He's an undergraduate of Computer Science and Engineering and Statistics from the University of California, Irvine (UCI). Previously, he played a role as a lead mentor at Artificial Intelligence at UCI, undergraduate researcher at Computer Vision Lab at UCI and software engineering intern at Google LLC. and Bentley Systems.
-				</p>
+		<p>
+			Based in Seattle, Omkar contributes to data center automations at Amazon Web Services (AWS). He's an undergraduate of Computer Science and Engineering and Statistics from the University of California, Irvine (UCI). Previously, he played a role as a lead mentor at Artificial Intelligence at UCI, undergraduate researcher at Computer Vision Lab at UCI and software engineering intern at Google LLC. and Bentley Systems.
+		</p>
 
-				<p>
-					Being a curious perpetual learner comes with an ambition to do a lot with limited time and energy. Omkar is currently prototyping <a href = "https://pasteOnEth.com">www.pasteOnEth.com</a>: POC for decentralized code repositories. He's also working on an Integrity Verification System protocol with an emphasis on Employee Background Verifications. Outside of that, his current interests are cryptography, user-experiences (UX), chess, running and more. 
-				</p>
+		<p>
+			Being a curious perpetual learner comes with an ambition to do a lot with limited time and energy. Omkar is currently prototyping <a href = "https://pasteOnEth.com">www.pasteOnEth.com</a>: POC for decentralized code repositories. He's also working on an Integrity Verification System protocol with an emphasis on Employee Background Verifications. Outside of that, his current interests are cryptography, user-experiences (UX), chess, running and more. 
+		</p>
 
-				<p> 
-				<div class="libre-baskerville-bold"> Digital Footprint </div>
-					<br/>
-					<li> 
-						<a href= "https://www.burkelab.com/wp-content/uploads/%E2%80%9CCloudStation%E2%80%9D-A-Cloud-based-Ground-Control-Station-for-Drones.pdf">
-						Publication</a>-  L. Hu et al., "CloudStation: A Cloud-Based Ground Control Station for Drones," in IEEE Journal on Miniaturization for Air and Space Systems. 
-					</li>
-					<li>
-						<a href= "https://engineering.uci.edu/news/2023/2/burke-achieves-distance-world-record-piloting-drone-through-internet">Article</a> titled "Burke Achieves Distance World Record for Piloting Drone through the Internet" for work on autonomous drones
-					</li>
-					<li>
-						<a href= "https://news.uci.edu/magazines/articles/in-the-know/">Article</a> and <a href= "https://issuu.com/ucimagazine/docs/uci_magazine">magazine</a> representing AI at UCI
-					</li>
-					<li>
-						Honors <a href="content/ics_honors_report.pdf"> thesis </a> advised by Charless C. Fowlkes 
-					</li>
-					<li>
-						<a href= "https://newuniversity.org/2017/11/07/60-second-science-trash-talk/">Article</a> on "Trash talk: a sustainable-fist waste bins project"
-					</li>
-					<li>
-						Independent Research on "Adaptive/Automated Watering System Prototyping" in 2017/18 with <a href= "https://sites.uci.edu/orange/quoc-viet-dang-contact/service-mentoring/">Professor Quoc Viet Dang</a>
-					</li>
-				</p>
+		<p> 
+		<div class="libre-baskerville-bold"> Digital Footprint </div>
+			<br/>
+			<li> 
+				<a href= "https://www.burkelab.com/wp-content/uploads/%E2%80%9CCloudStation%E2%80%9D-A-Cloud-based-Ground-Control-Station-for-Drones.pdf">
+				Publication</a>-  L. Hu et al., "CloudStation: A Cloud-Based Ground Control Station for Drones," in IEEE Journal on Miniaturization for Air and Space Systems. 
+			</li>
+			<li>
+				<a href= "https://engineering.uci.edu/news/2023/2/burke-achieves-distance-world-record-piloting-drone-through-internet">Article</a> titled "Burke Achieves Distance World Record for Piloting Drone through the Internet" for work on autonomous drones
+			</li>
+			<li>
+				<a href= "https://news.uci.edu/magazines/articles/in-the-know/">Article</a> and <a href= "https://issuu.com/ucimagazine/docs/uci_magazine">magazine</a> representing AI at UCI
+			</li>
+			<li>
+				Honors <a href="content/ics_honors_report.pdf"> thesis </a> advised by Charless C. Fowlkes 
+			</li>
+			<li>
+				<a href= "https://newuniversity.org/2017/11/07/60-second-science-trash-talk/">Article</a> on "Trash talk: a sustainable-fist waste bins project"
+			</li>
+			<li>
+				Independent Research on "Adaptive/Automated Watering System Prototyping" in 2017/18 with <a href= "https://sites.uci.edu/orange/quoc-viet-dang-contact/service-mentoring/">Professor Quoc Viet Dang</a>
+			</li>
+		</p>
 
-				<p> 
-					If you would like to connect, you can find me on <a href="https://www.linkedin.com/in/omkar-pathak/" > LinkedIn </a> 
-				</p> 
-				`,
-				"Footer" : `Footer text  for page 1`
+		<p> 
+			If you would like to connect, you can find me on <a href="https://www.linkedin.com/in/omkar-pathak/" > LinkedIn </a> 
+		</p> 
+		`
 	},
 	
-	"Content": {	"Header" : "Headeline 2 ",
-				"Body": 
-				` 
-				<p> Here is a curated list of content that has influenced me. </p>
+	"Content": {	
+		"Body": 
+		` 
+		<p> Here is a curated list of content that has influenced me. </p>
 
-				<p>
-				    <div class="libre-baskerville-bold"> Books </div> 
-					<br/>
-					<li> Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones by James Clear </li>
-					<li> The Psychology of Money by Morgan Housel </li>
-					<li> Talking to strangers: What We Should Know About the People We Don't Know by Malcolm Gladwell </li>
-					<li> Indigenous: Growing up Californian by Cris Mazza </li> 
-					<li> The Vanishing Half by Britt Bennett </li>
-					<li> Token Economics: How the Web3 reinvents the Internet by Shermin Voshmgir </li> 
-					<li> A Promised Land by Barack Obama </li> 
-				</p>
+		<p>
+			<div class="libre-baskerville-bold"> Books </div> 
+			<br/>
+			<li> Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones by James Clear </li>
+			<li> The Psychology of Money by Morgan Housel </li>
+			<li> Talking to strangers: What We Should Know About the People We Don't Know by Malcolm Gladwell </li>
+			<li> Indigenous: Growing up Californian by Cris Mazza </li> 
+			<li> The Vanishing Half by Britt Bennett </li>
+			<li> Token Economics: How the Web3 reinvents the Internet by Shermin Voshmgir </li> 
+			<li> A Promised Land by Barack Obama </li> 
+		</p>
 
-				<p>
-					<div class="libre-baskerville-bold"> Videos </div> 
-					<br/>
-					<li> Cryptocurrencies: Last Week Tonight with John Oliver (HBO) 
-						<a href="https://www.youtube.com/watch?v=g6iDZspbRMg"> link </a> 
-					</li>
-					<li> How To Steal Like An Artist 
-						<a href="https://www.youtube.com/watch?v=lmso66PVGtU"> link </a> 
-					</li>
-					<li> Sonam Wangchuk, Rolex Awards Laureate, 2016 
-						<a href="https://www.youtube.com/watch?v=FdVijr10DZ0&list=PLUhqj3ulUmAalTpnKu4jNvOfBshI6xxsu&index=4&t=1s"> link </a> 
-					</li>
-					<li> Jeff Bezos: The electricity metaphor 
-						<a href="https://www.youtube.com/watch?v=vMKNUylmanQ&list=WL&index=15&t=51s"> link </a> 
-					</li>
-					<li> Conan O'Brien in conversation with Harvard University President Drew Faust 
-						<a href="https://www.youtube.com/watch?v=gCVZIpxUEhg&list=WL&index=12&t=1294s"> link </a> 
-					</li>
-					<li> Why do prime numbers make these spirals?  
-						<a href="https://www.youtube.com/watch?v=EK32jo7i5LQ"> link </a> 
-					</li>
-					<li> How Termites Inspired a Building That Can Cool Itself  
-						<a href="https://www.youtube.com/watch?v=620omdSZzBs"> link </a> 
-					</li>
-				</p>
+		<p>
+			<div class="libre-baskerville-bold"> Videos </div> 
+			<br/>
+			<li> Cryptocurrencies: Last Week Tonight with John Oliver (HBO) 
+				<a href="https://www.youtube.com/watch?v=g6iDZspbRMg"> link </a> 
+			</li>
+			<li> How To Steal Like An Artist 
+				<a href="https://www.youtube.com/watch?v=lmso66PVGtU"> link </a> 
+			</li>
+			<li> Sonam Wangchuk, Rolex Awards Laureate, 2016 
+				<a href="https://www.youtube.com/watch?v=FdVijr10DZ0&list=PLUhqj3ulUmAalTpnKu4jNvOfBshI6xxsu&index=4&t=1s"> link </a> 
+			</li>
+			<li> Jeff Bezos: The electricity metaphor 
+				<a href="https://www.youtube.com/watch?v=vMKNUylmanQ&list=WL&index=15&t=51s"> link </a> 
+			</li>
+			<li> Conan O'Brien in conversation with Harvard University President Drew Faust 
+				<a href="https://www.youtube.com/watch?v=gCVZIpxUEhg&list=WL&index=12&t=1294s"> link </a> 
+			</li>
+			<li> Why do prime numbers make these spirals?  
+				<a href="https://www.youtube.com/watch?v=EK32jo7i5LQ"> link </a> 
+			</li>
+			<li> How Termites Inspired a Building That Can Cool Itself  
+				<a href="https://www.youtube.com/watch?v=620omdSZzBs"> link </a> 
+			</li>
+		</p>
 
-				<p> 
-					<div class="libre-baskerville-bold"> Online Literature </div> 
-					<br/>
-					<li> Impact of open source and AI in 2023 
-						<a href="https://github.blog/2023-11-08-the-state-of-open-source-and-ai/?utm_source=banner&utm_medium=github&utm_campaign=octoverse"> link </a> 
-					</li>
-					<li> The Techno-Optimist Manifesto by Marc Andreessen 
-						<a href="https://a16z.com/the-techno-optimist-manifesto/"> link </a> 
-					</li>
-					<li> Facial Recognition and Policing in America by Georgetown Law University
-						<a href="https://www.law.georgetown.edu/privacy-technology-center/publications/the-perpetual-line-up/"> link </a> 
-					</li>
-					<li> 
-					<a href="https://spectrum.ieee.org/history-of-money"> History</a>  of money and 
-					<a href="https://www.bankofengland.co.uk/-/media/boe/files/speech/2018/the-future-of-money-speech-by-mark-carney.pdf?la=en&"> Future</a>  of money 
-					</li>
-					<li> Bitcoin P2P e-cash paper <a href="https://www.metzdowd.com/pipermail/cryptography/2008-October/014810.html"> link </a></li>
-					<li> Doughlas Adams and the cult of 42 <a href="https://www.theguardian.com/books/2011/feb/03/douglas-adams-42-hitchhiker"> link </a> 
-					</li>
-				</p>
-				`,
-				"Footer" : `Footer text for page 2`
+		<p> 
+			<div class="libre-baskerville-bold"> Online Literature </div> 
+			<br/>
+			<li> Impact of open source and AI in 2023 
+				<a href="https://github.blog/2023-11-08-the-state-of-open-source-and-ai/?utm_source=banner&utm_medium=github&utm_campaign=octoverse"> link </a> 
+			</li>
+			<li> The Techno-Optimist Manifesto by Marc Andreessen 
+				<a href="https://a16z.com/the-techno-optimist-manifesto/"> link </a> 
+			</li>
+			<li> Facial Recognition and Policing in America by Georgetown Law University
+				<a href="https://www.law.georgetown.edu/privacy-technology-center/publications/the-perpetual-line-up/"> link </a> 
+			</li>
+			<li> 
+			<a href="https://spectrum.ieee.org/history-of-money"> History</a>  of money and 
+			<a href="https://www.bankofengland.co.uk/-/media/boe/files/speech/2018/the-future-of-money-speech-by-mark-carney.pdf?la=en&"> Future</a>  of money 
+			</li>
+			<li> Bitcoin P2P e-cash paper <a href="https://www.metzdowd.com/pipermail/cryptography/2008-October/014810.html"> link </a></li>
+			<li> Doughlas Adams and the cult of 42 <a href="https://www.theguardian.com/books/2011/feb/03/douglas-adams-42-hitchhiker"> link </a> 
+			</li>
+		</p>
+		`
 	}		
 	
 }
